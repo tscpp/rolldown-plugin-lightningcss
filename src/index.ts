53125f66f154948f97211887a2bcbcb62ce59e7b
Bartosz Szczecinski <bartosz.szczecinski@outlook.com>
2026-08-10 18:02:53 +0200

    feat: add custom transformer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { RolldownMagicString, type Plugin } from "rolldown";
-import { bundleAsync, type CustomAtRules, type TransformOptions } from "lightningcss";
+import {
+  bundleAsync,
+  type CustomAtRules,
+  type TransformOptions,
+} from "lightningcss";
 import sourceMapRemapping from "@jridgewell/remapping";
 import { basename, dirname, relative } from "node:path/posix";
 
@@ -12,25 +16,35 @@ const URL_PLACEHOLDER_REGEXP = new RegExp(
   "g",
 );
 
-export interface LightningcssOptions<C extends CustomAtRules> extends Pick<
-  TransformOptions<C>,
-  | "customAtRules"
-  | "drafts"
-  | "errorRecovery"
-  | "exclude"
-  | "include"
-  | "minify"
-  | "nonStandard"
-  | "pseudoClasses"
-  | "targets"
-  | "unusedSymbols"
-  | "visitor"
-  | "sourceMap"
-> {}
+type TransformerInterface = {
+  /**Provide a custom transformer instead of lightningcss, useful if you want to include CSS as-is */
+  transformer?: (css: string) => string;
+};
+
+export interface LightningcssOptions<C extends CustomAtRules>
+  extends
+    TransformerInterface,
+    Pick<
+      TransformOptions<C>,
+      | "customAtRules"
+      | "drafts"
+      | "errorRecovery"
+      | "exclude"
+      | "include"
+      | "minify"
+      | "nonStandard"
+      | "pseudoClasses"
+      | "targets"
+      | "unusedSymbols"
+      | "visitor"
+      | "sourceMap"
+    > {}
 
 const decoder = new TextDecoder();
 
-export function lightningcss<C extends CustomAtRules>(options?: LightningcssOptions<C>): Plugin {
+export function lightningcss<C extends CustomAtRules>(
+  options: LightningcssOptions<C> = {},
+): Plugin {
   return {
     name: "lightningcss",
 
@@ -40,87 +54,108 @@ export function lightningcss<C extends CustomAtRules>(options?: LightningcssOpti
         moduleType: ["css"],
       },
       async handler(inputCode, id) {
-        // --- 1. Bundle code using lightningcss ----------------
-
-        const bundled = await bundleAsync({
-          filename: id,
-          resolver: {
-            read: (file) => {
-              if (file === id) return inputCode;
-              return this.fs.readFile(file, { encoding: "utf8" });
+        let magicString: RolldownMagicString;
+        let finalMap: string | null = "";
+        if (options.transformer) {
+          const fileContents = await this.fs.readFile(id, { encoding: "utf8" });
+          const code = options.transformer(fileContents.toString());
+          magicString = new RolldownMagicString(
+            code.replaceAll("`", "\\`").replaceAll("${", "\\${"),
+            { filename: id },
+          );
+        } else {
+          // --- 1. Bundle code using lightningcss ----------------
+          const bundled = await bundleAsync({
+            filename: id,
+            resolver: {
+              read: (file) => {
+                if (file === id) return inputCode;
+                return this.fs.readFile(file, { encoding: "utf8" });
+              },
+              resolve: async (id, importer) => {
+                const resolved = await this.resolve(id, importer);
+                if (!resolved) {
+                  throw this.error(`Unable to resolve module '${id}'.`);
+                }
+                if (resolved.external) {
+                  return { external: resolved.id } as any;
+                }
+                return resolved.id;
+              },
             },
-            resolve: async (id, importer) => {
-              const resolved = await this.resolve(id, importer);
-              if (!resolved) {
-                throw this.error(`Unable to resolve module '${id}'.`);
-              }
-              if (resolved.external) {
-                return { external: resolved.id } as any;
-              }
-              return resolved.id;
+            analyzeDependencies: {
+              preserveImports: true,
             },
-          },
-          analyzeDependencies: {
-            preserveImports: true,
-          },
-          ...options,
-        });
+            ...options,
+          });
 
-        const draftCss = decoder
-          .decode(bundled.code)
-          .replaceAll("`", "\\`")
-          .replaceAll("${", "\\${");
+          const draftCss = decoder
+            .decode(bundled.code)
+            .replaceAll("`", "\\`")
+            .replaceAll("${", "\\${");
 
-        // --- 2. Resolve dependencies using magic string -----------
+          // --- 2. Resolve dependencies using magic string -----------
 
-        const magicString = new RolldownMagicString(draftCss, { filename: id });
+          magicString = new RolldownMagicString(draftCss, { filename: id });
 
-        for (const dependency of bundled.dependencies!) {
-          switch (dependency.type) {
-            case "url": {
-              if (dependency.url.startsWith('data:') || dependency.url.startsWith('http:') || dependency.url.startsWith('https:')) {
+          for (const dependency of bundled.dependencies!) {
+            switch (dependency.type) {
+              case "url": {
+                if (
+                  dependency.url.startsWith("data:") ||
+                  dependency.url.startsWith("http:") ||
+                  dependency.url.startsWith("https:")
+                ) {
+                  magicString.replace(dependency.placeholder, dependency.url);
+                  break;
+                }
+                const resolved = await this.resolve(dependency.url, id, {
+                  kind: "new-url",
+                });
+                if (!resolved) {
+                  throw this.error(
+                    `Unable to resolve '${dependency.url}'.`,
+                    dependency.loc.start,
+                  );
+                }
+
+                const content = await this.fs.readFile(resolved.id);
+
+                const referenceId = this.emitFile({
+                  type: "asset",
+                  name: basename(resolved.id),
+                  source: content,
+                });
+
+                magicString.replace(
+                  dependency.placeholder,
+                  getUrlPlaceholder(referenceId),
+                );
+                break;
+              }
+
+              case "import": {
                 magicString.replace(dependency.placeholder, dependency.url);
                 break;
-              }              
-              const resolved = await this.resolve(dependency.url, id, { kind: "new-url" });
-              if (!resolved) {
-                throw this.error(`Unable to resolve '${dependency.url}'.`, dependency.loc.start);
               }
 
-              const content = await this.fs.readFile(resolved.id);
-
-              const referenceId = this.emitFile({
-                type: "asset",
-                name: basename(resolved.id),
-                source: content,
-              });
-
-              magicString.replace(dependency.placeholder, getUrlPlaceholder(referenceId));
-              break;
+              default:
+                this.error(`Unsupported dependency '${dependency.type}'.`);
             }
-
-            case "import": {
-              magicString.replace(dependency.placeholder, dependency.url);
-              break;
-            }
-
-            default:
-              this.error(`Unsupported dependency '${dependency.type}'.`);
           }
+          finalMap = options?.sourceMap
+            ? sourceMapRemapping(
+                [
+                  magicString.generateMap().toString(),
+                  decoder.decode(bundled.map!),
+                ],
+                () => null,
+              ).toString()
+            : null;
         }
 
-        // --- 3. Combine source maps ------------
-
         const finalCss = magicString.toString();
-
-        const finalMap = options?.sourceMap
-          ? sourceMapRemapping(
-              [magicString.generateMap().toString(), decoder.decode(bundled.map!)],
-              () => null,
-            ).toString()
-          : null;
-
-        // --- 4. Convert to JavaScript ---------------------
+        // --- 3. Convert to JavaScript ---------------------
 
         const info = this.getModuleInfo(id);
         const isEntry = info?.isEntry;
@@ -146,8 +181,9 @@ export function lightningcss<C extends CustomAtRules>(options?: LightningcssOpti
           chunk.facadeModuleId.endsWith(".css") &&
           chunk.code.includes(ENTRY_HINT)
         ) {
-          const code = chunk.code.replaceAll(URL_PLACEHOLDER_REGEXP, (...args) =>
-            toRelative(this.getFileName(args[1]), chunk.fileName),
+          const code = chunk.code.replaceAll(
+            URL_PLACEHOLDER_REGEXP,
+            (...args) => toRelative(this.getFileName(args[1]), chunk.fileName),
           );
           const dataUrl = `data:text/javascript;base64,${Buffer.from(code).toString("base64")}`;
           const exports = await import(dataUrl);
