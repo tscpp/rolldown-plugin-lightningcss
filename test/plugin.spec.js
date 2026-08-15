8da27ef8d8c205a08b48c30d7b19083b6d41c2b7
Elias Skogevall <elias.skogevall@gmail.com>
2026-08-15 10:31:18 +0200

    test: supports custom transformations

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -49,3 +49,60 @@ it("preserves externals urls", async () => {
   assert(source.includes("https://example.com/bg.png"));
   assert(source.includes("data:foo"));
 });
+
+it("supports pre transformations", async () => {
+  const result = await rolldown({
+    input: ["test/fixtures/preprocess.css"],
+    plugins: [
+      {
+        name: "my-css-pre-processing",
+        transform: {
+          order: "pre",
+          filter: {
+            moduleType: ["css"],
+          },
+          async handler(code) {
+            return code.replaceAll("\\", "\\\\");
+          },
+        },
+      },
+      lightningcss(),
+    ],
+  });
+  const { output } = await result.generate();
+  const source = output.find((asset) => asset.fileName === "preprocess.css").source.toString();
+  assert(source.includes("\\00ba"));
+});
+
+it("supports lightningcss' visitors", async () => {
+  const result = await rolldown({
+    input: ["test/fixtures/preprocess.css"],
+    plugins: [
+      lightningcss({
+        visitor: {
+          Token: (token) => {
+            if (token.type === "string") {
+              const value = Array.from(token.value)
+                .map((char) => {
+                  const p = char.charCodeAt(0);
+                  return p <= 0x7f ? char : `\\\\${p.toString(16).padStart(4, "0")}`;
+                })
+                .join("");
+              return {
+                type: "token",
+                value: {
+                  type: "string",
+                  value: value,
+                },
+                raw: `'${value.replaceAll("'", "\\'").replaceAll("\\", "\\\\")}'`,
+              };
+            }
+          },
+        },
+      }),
+    ],
+  });
+  const { output } = await result.generate();
+  const source = output.find((asset) => asset.fileName === "preprocess.css").source.toString();
+  assert(source.includes("\\00ba"));
+});
